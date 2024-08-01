6d16ad9cf029a7d6e722a57c5761d30228f4d130
AL66 <andreas.leinen@imicros.de>
2024-08-01 20:25:50 +0200

    Sync

--- local.js
+++ local.js
@@ -11,7 +11,7 @@ process.env.CASSANDRA_PORT = 31326;
 process.env.CASSANDRA_USER = "cassandra";
 process.env.CASSANDRA_PASSWORD = "cassandra";
 // ---- KAFKA ----
-process.env.KAFKA_BROKER = "192.168.2.124:30088";
+process.env.KAFKA_BROKER = "192.168.2.124:32061";
 process.env.KAFKAJS_NO_PARTITIONER_WARNING = "1";
 // ---- NATS ----
 process.env.NATS_TRANSPORTER = "nats://192.168.2.124:30284";
@@ -19,8 +19,8 @@ process.env.NATS_TRANSPORTER = "nats://192.168.2.124:30284";
 process.env.MINIO_ENDPOINT = "192.168.2.124";
 process.env.MINIO_PORT = 30968; //9400;
 process.env.MINIO_NO_SSL = "true";
-process.env.MINIO_ACCESS_KEY = "minio"; // "AKIAIOSFODNN7EXAMPLE";
-process.env.MINIO_SECRET_KEY = "minio123"; // "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+process.env.MINIO_ACCESS_KEY = "8vWIDvp0xQNoXGE2pCP3"; //"minio";
+process.env.MINIO_SECRET_KEY = "6VnXqP6b4BEqNHnUnIIU4OfCXWBcXn5E7CPUftRt"; // "minio123";
 // ---- Vault init token ----
 process.env.MASTER_TOKEN = "074e48c8e3c0bc19f9e22dd7570037392"; //crypto.randomBytes(32).toString("hex");
 // ---- EXCHANGE ----
